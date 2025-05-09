62fb4daf4667df21950637ccdd187110ec103fc5
root <root@localhost.localdomain>
2025-05-09 05:00:18 -0500

    envio usando variable de entorno ambas

--- src/app/service/email.service.ts
+++ src/app/service/email.service.ts
@@ -7,9 +7,49 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class EmailService {
+/*   private apiUrl = 'https://db.redpsicologos.cl:5542/welcome';
+ */
+/* private apiUrl = '/email-api/welcome';  // Usa la ruta del proxy
+  constructor(public http: HttpClient) {} */
+  private apiUrl: string;
+  constructor(public http: HttpClient) {
+    // Si está en producción, usa la URL del entorno
+    // Si está en desarrollo, usa la ruta del proxy
+    this.apiUrl = environment.production 
+      ? environment.emailServiceUrl 
+      : '/email-api/welcome';
+    console.log('Email Service URL:', this.apiUrl);
+  }
 
-private apiUrl = environment.emailServiceUrl;  // Usa la ruta del proxy
-  constructor(public http: HttpClient) {}
+  /* sendWelcomeEmail(toEmail: string, toName: string, templateId: number, params: any): Observable<any> {
+    const body = {
+      toEmail,
+      toName,
+      templateId,
+      params
+    };
+    return this.http.post(this.apiUrl, body);
+  } */
+  /* sendWelcomeEmail(toEmail: string, toName: string, templateId: number, params: any): Observable<any> {
+    const body = {
+      toEmail,
+      toName,
+      templateId,
+      params
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+
+    return this.http.post(this.apiUrl, body, { headers }).pipe(
+      catchError(error => {
+        console.error('Error en el servicio de email:', error);
+        return throwError(() => new Error('Error al enviar el correo de bienvenida'));
+      })
+    );
+  } */
 
     sendWelcomeEmail(toEmail: string, toName: string, templateId: number, params: any): Observable<any> {
       const body = {
