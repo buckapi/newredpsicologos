46266bbc56bbfdc898b7125cc23be32ab78c3433
root <root@localhost.localdomain>
2025-05-09 04:41:27 -0500

    envio usando variable de entorno

--- src/app/service/email.service.ts
+++ src/app/service/email.service.ts
@@ -1,45 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
-/*   private apiUrl = 'https://db.redpsicologos.cl:5542/welcome';
- */
-private apiUrl = '/email-api/welcome';  // Usa la ruta del proxy
+
+private apiUrl = environment.emailServiceUrl;  // Usa la ruta del proxy
   constructor(public http: HttpClient) {}
-
-  /* sendWelcomeEmail(toEmail: string, toName: string, templateId: number, params: any): Observable<any> {
-    const body = {
-      toEmail,
-      toName,
-      templateId,
-      params
-    };
-    return this.http.post(this.apiUrl, body);
-  } */
-  /* sendWelcomeEmail(toEmail: string, toName: string, templateId: number, params: any): Observable<any> {
-    const body = {
-      toEmail,
-      toName,
-      templateId,
-      params
-    };
-
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    });
-
-    return this.http.post(this.apiUrl, body, { headers }).pipe(
-      catchError(error => {
-        console.error('Error en el servicio de email:', error);
-        return throwError(() => new Error('Error al enviar el correo de bienvenida'));
-      })
-    );
-  } */
 
     sendWelcomeEmail(toEmail: string, toName: string, templateId: number, params: any): Observable<any> {
       const body = {
