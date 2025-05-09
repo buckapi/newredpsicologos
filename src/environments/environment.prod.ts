905cab506660a1e2c68be19ebd049da7d6ff9f6e
root <root@localhost.localdomain>
2025-05-09 04:49:14 -0500

    envio usando variable de entorno prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   recaptchaSiteKey: '6Leeui0rAAAAAMyUKhrnhgGwJDajrKJ8yk3FZK7T', 
-  recaptchaSecretKey: '6Leeui0rAAAAAFPGCAomijcMLh0ewkApJvXxcB5d' 
+  recaptchaSecretKey: '6Leeui0rAAAAAFPGCAomijcMLh0ewkApJvXxcB5d',
+  emailServiceUrl: 'https://db.redpsicologos.cl:5542/welcome'
+
 };
